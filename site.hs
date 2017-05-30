95babad4437bece8a725e8b3cfa10fade6b9ccae
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-05-30 09:10:42 +0200

    Start migration to materialize

--- site.hs
+++ site.hs
@@ -8,62 +8,57 @@ import Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main =
-  hakyllWith (defaultConfiguration {deployCommand = "rsync -av _site/ us:html"}) $
-  do match "images/*" $
-       do route idRoute
-          compile copyFileCompiler
-     match "css/*" $
-       do route idRoute
-          compile compressCssCompiler
-     match (fromList ["about.md","contact.md"]) $
-       do route $ setExtension "html"
-          compile $
-            pandocCompiler >>=
-            loadAndApplyTemplate "templates/default.html" defaultContext >>=
-            relativizeUrls
-     match "posts/*" $
-       do route $ setExtension "html"
-          compile $
-            pandocCompilerWith defaultHakyllReaderOptions
-                               (defaultHakyllWriterOptions {writerHtml5 = True})
-            >>=
-            loadAndApplyTemplate "templates/post.html" postCtx >>=
-            saveSnapshot "content" >>=
-            loadAndApplyTemplate "templates/default.html" postCtx >>=
-            relativizeUrls
-     create ["archive.html"] $
-       do route idRoute
-          compile $
-            do posts <-
-                 recentFirst =<< loadAll "posts/*"
-               let archiveCtx =
-                     listField "posts" postCtx (return posts) `mappend`
-                     constField "title" "Archives" `mappend`
-                     defaultContext
-               makeItem "" >>=
-                 loadAndApplyTemplate "templates/archive.html" archiveCtx >>=
-                 loadAndApplyTemplate "templates/default.html" archiveCtx >>=
-                 relativizeUrls
-     match "index.html" $
-       do route idRoute
-          compile $
-            do posts <-
-                 recentFirst =<< loadAll "posts/*"
-               let indexCtx =
-                     listField "posts" postCtx (return posts) `mappend`
-                     defaultContext
-               getResourceBody >>= applyAsTemplate indexCtx >>=
-                 loadAndApplyTemplate "templates/default.html" indexCtx >>=
-                 relativizeUrls
-     match "templates/*" $ compile templateCompiler
-     create ["atom.xml"] $
-       do route idRoute
-          compile $
-            do let feedCtx =
-                     postCtx `mappend` bodyField "description"
-               posts <-
-                 recentFirst =<< loadAllSnapshots "posts/*" "content"
-               renderAtom myFeedConfiguration feedCtx posts
+  hakyllWith (defaultConfiguration {deployCommand = "rsync -av _site/ us:html"}) $ do
+    match "images/*" $ do
+      route idRoute
+      compile copyFileCompiler
+    match "css/*" $ do
+      route idRoute
+      compile compressCssCompiler
+    match (fromList ["about.md", "contact.md"]) $ do
+      route $ setExtension "html"
+      compile $
+        pandocCompiler >>=
+        loadAndApplyTemplate "templates/default.html" defaultContext >>=
+        relativizeUrls
+    match "posts/*" $ do
+      route $ setExtension "html"
+      compile $
+        pandocCompilerWith
+          defaultHakyllReaderOptions
+          (defaultHakyllWriterOptions
+           {writerHtml5 = True, writerHighlight = False}) >>=
+        loadAndApplyTemplate "templates/post.html" postCtx >>=
+        saveSnapshot "content" >>=
+        loadAndApplyTemplate "templates/default.html" postCtx >>=
+        relativizeUrls
+    create ["archive.html"] $ do
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let archiveCtx =
+              listField "posts" postCtx (return posts) `mappend`
+              constField "title" "Archives" `mappend`
+              defaultContext
+        makeItem "" >>= loadAndApplyTemplate "templates/archive.html" archiveCtx >>=
+          loadAndApplyTemplate "templates/default.html" archiveCtx >>=
+          relativizeUrls
+    match "index.html" $ do
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let indexCtx =
+              listField "posts" postCtx (return posts) `mappend` defaultContext
+        getResourceBody >>= applyAsTemplate indexCtx >>=
+          loadAndApplyTemplate "templates/default.html" indexCtx >>=
+          relativizeUrls
+    match "templates/*" $ compile templateCompiler
+    create ["atom.xml"] $ do
+      route idRoute
+      compile $ do
+        let feedCtx = postCtx `mappend` bodyField "description"
+        posts <- recentFirst =<< loadAllSnapshots "posts/*" "content"
+        renderAtom myFeedConfiguration feedCtx posts
 
 
 --------------------------------------------------------------------------------
