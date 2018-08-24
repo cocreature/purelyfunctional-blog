c3e8b948c15976cfbc68331a76bf26d1a56f136e
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2018-08-24 15:24:59 +0200

    Remove stalk config and update nix config

--- default.nix
+++ default.nix
@@ -1,16 +1,17 @@
-{ rev ? "090b7cc8f1bae880fc5542987ede2bcb444d20bf",
-  outputSha256 ? "0n23hlbg5np3882qbr2wbh7nz5wvnljszy0899cv628aqqrmym6i"
+{ rev ? "beaa0984c63f6e93a9ddfae18c70d39602e90cca",
+  outputSha256 ? "04lnkb5zvks7x7vvx8d0k0vnwx4hv51zh520fqxphhnkazvz0bxd"
 }:
+
 let
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     sha256 = outputSha256;
   };
   pkgs = import nixpkgs {};
-  haskellPackages = (pkgs.haskell.packages.ghc842.extend (pkgs.haskell.lib.packageSourceOverrides {
-    blog = ./.;
-  }));
+  haskellPackages = pkgs.haskell.packages.ghc843.override (oldAttrs: {
+    overrides = self: super: {
+      blog = super.callCabal2nix "blog" ./. {};
+    };
+  });
 in
-haskellPackages.shellFor {
-  packages = p: [p.blog];
-}
+haskellPackages.blog
